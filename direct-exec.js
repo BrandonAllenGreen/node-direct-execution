a6ceb1215f51715ade8a61fd3f1dde97c5e1d84a
BrandonAllenGreen <green04@gmail.com>
2018-03-03 18:55:59 -0600

    this works the windows way

---
+++ direct-exec.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+'use strict';
+
+
+// The file should contain JavaScript code to output the phrase 
+// "Hello, World. I was executed without the 'node' command!" 
+// to the terminal through the stdout stream.
+
+
+process.stdout.write("Hello, World. I was executed without the 'node' command!");
